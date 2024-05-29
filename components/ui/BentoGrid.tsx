90da5abd069efd2008dc9f728697e71e60859faa
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-05-29 15:03:14 +0530

    edit portfolio

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -136,6 +136,18 @@ export const BentoGridItem = ({
       </div>
 
 {id=== 2 && <GlobeDemo />}
+
+{id === 3 && (
+  <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+    <div className="flex flex-col gap-3 lg:gap-8">
+      {['React.js', 'Next.js', 'TypeScript'].map((item) => (
+<span key={item} className="py-2 ">
+  {item}
+</span>
+      ))}
+      </div>
+    </div>
+) }
     </div>
   )
 };
