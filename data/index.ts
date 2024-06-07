d058eb806e99d5d51fe809b968156ccba0b7e3ec
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-07 11:26:27 +0530

    change styles

--- data/index.ts
+++ data/index.ts
@@ -22,7 +22,7 @@ export const gridItems = [
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
-    titleClassName: "justify-start",
+    titleClassName: "justify-start ",
     img: "",
     spareImg: "",
   },
