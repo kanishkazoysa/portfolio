15ee7bc03750fe61acc5e1b03f480fb75abd316a
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-04 00:55:04 +0530

    edit styles

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -144,19 +144,19 @@ export const BentoGridItem = ({
         {id === 2 && <GlobeDemo />}
 
         {id === 3 && (
-          <div className="flex gap-1 lg:gap-3 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-2">
-              {['React.js', 'Next.js', 'TypeScript'].map((item) => (
-                <span key={item} className="py-2 lg:py-3 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+          <div className="flex gap-1 lg:gap-3 w-fit absolute -right-3 lg:right-1">
+            <div className="flex flex-col gap-3 lg:gap-2 mt-3">
+              {['React.js', 'Next.js', 'TypeScript', 'JavaScript', 'C#', ''].map((item) => (
+                <span key={item} className="py-2 lg:py-3 lg:px-4 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
               ))}
               <span className="py-7 px-3 rounded-lg text-center bg-[#10132e]" />
             </div>
-            <div className="flex flex-col gap-3 lg:gap-2 mb-4">
+            <div className="flex flex-col gap-3 lg:gap-2 mb-2">
               <span className="py-5 px-3 rounded-lg text-center bg-[#10132e]" />
-              {['VueJS', 'AWS', 'MongoDB'].map((item) => (
-                <span key={item} className="py-2 lg:py-3 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+              {['VueJS', 'AWS', 'MongoDB', 'MySQL', 'NodeJs', '.NET'].map((item) => (
+                <span key={item} className="py-2 lg:py-3 lg:px-4 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
               ))}
