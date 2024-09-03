d934f5dd868adc9bb36c0c96ef578f616d62cf88
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-04 03:51:59 +0530

    setup sentry

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
     return (
-        <div className='py-20'>
+        <div className='py-20' id="projects">
             <h1 className='heading'>
                 A small selection of {''}
                 <span className='text-purple'>recent projects</span>
@@ -13,7 +13,7 @@ const RecentProjects = () => {
             <div className='flex flex-wrap items-center 
       justify-center p-4 gap-x-24 gap-y-8 mt-10'>
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
-                    <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] h-[25rem] flex items-center
+                    <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center
                      justify-center sm:w-[570px] w-[80vw]'>
                         <PinContainer title={link} href={link}>
                             <div className='relative flex items-center justify-center
