5e7c971dd404a7c2b69d4c9c0b179b94962e8dc2
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-06 04:46:00 +0530

    update footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,9 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10 relative" id="contact">
       <div className="flex flex-col md:flex-row justify-between items-start pb-40">
+        <div className="block md:hidden w-full h-[300px] mb-10"> {/* Visible only on small devices */}
+          <GlobeDemo />
+        </div>
         <div className="flex flex-col items-start w-full md:w-1/2 z-10">
           <h1 className="heading lg:max-w-[45vw] items-center md:text-left">
             Ready to take <span className="text-purple">your</span> digital
@@ -27,13 +30,13 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="md:w-1/2 h-[500px] absolute right-0 top-0 bottom-0">
+        <div className="hidden md:mt-5 md:block md:w-1/2 h-[500px] absolute right-0 top-0 bottom-0"> {/* Hidden on small devices */}
           <GlobeDemo />
         </div>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center relative z-10">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
+      <div className="flex mt-16  md:flex-row flex-col justify-between items-center relative z-10">
+        <p className="md:text-base text-sm md:font-normal font-light mb-2">
+          Copyright © 2024 Kanishka Zoysa
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
