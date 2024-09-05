3dcde2f99e17d462a744cc2aa36f1477f5da4563
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-05 18:30:30 +0530

    edit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,35 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
+import GlobeDemo from "./ui/GridGlobeFooter";
 import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
-        </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
-        </p>
-        <a href="mailto:kanishkazoysa1234@gmail.com">
-          <MagicButton
-            title="Let's get in touch"
-            icon={<FaLocationArrow />}
-            position="right"
-          />
-        </a>
+    <footer className="w-full pt-20 pb-10 relative" id="contact">
+      <div className="flex flex-col md:flex-row justify-between items-center pb-40">
+        <div className="flex flex-col items-center md:items-start md:w-1/2 z-10">
+          <h1 className="heading lg:max-w-[45vw]">
+            Ready to take <span className="text-purple">your</span> digital
+            presence to the next level?
+          </h1>
+          <p className="text-white-200 md:mt-10 my-5 text-center md:text-left">
+            Reach out to me today and let&apos;s discuss how I can help you
+            achieve your goals.
+          </p>
+          <a href="mailto:kanishkazoysa1234@gmail.com">
+            <MagicButton
+              title="Let's get in touch"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+
+        <div className="md:w-1/2 h-[500px] absolute right-0 top-0 bottom-0">
+          <GlobeDemo />
+        </div>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center relative z-10">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Adrian Hajdin
         </p>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from 'react-icons/fa';
 
 const Hero = () => {
     return (
-        <div className='pb-20 pt-36 h-[100vh]'>
+        <div className='pb-20 pt-36'>
             <div>
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
@@ -16,13 +16,13 @@ const Hero = () => {
             <div className="h-screen w-full dark:bg-black-100 bg-white  
                 dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex 
                 items-center justify-center absolute top-0 left-0">
-                <div className="absolute pointer-events-none inset-0 
+                <div className="absolute pointer-events-none inset-0
                 flex items-center justify-center dark:bg-black-100 bg-white 
                 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
 
-            <div className='flex flex-row'>
-                <div className='flex relative my-20 z-10 flex-1'>
+            <div className='flex flex-row w-full'>
+                <div className='flex relative my-20 z-10 flex-1 w-1/3'>
                     <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col'>
                         <TextGenerateEffect
                             className='text-[10px] md:text-3xl lg:text-4xl'
@@ -44,8 +44,8 @@ const Hero = () => {
                         </a>
                     </div>
                 </div>
-                <div className='flex relative my-20 z-10 flex-1 bg-[url("../public/bg-explosion.jpg")] bg-cover bg-center'>
-                    image
+                <div className='flex relative my-20 z-10 flex-1 w-2/3 '>
+                    {/* Content here */}
                 </div>
             </div>
         </div>
