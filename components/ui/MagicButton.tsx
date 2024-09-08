a33b86663050fcdbeb31f8981d043770ada53d03
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-09 00:53:44 +0530

    edit

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const MagicButton = ({ title, icon, position, handleClick, otherClasses }: {
     title: string,
@@ -7,15 +7,15 @@ const MagicButton = ({ title, icon, position, handleClick, otherClasses }: {
     handleClick?: () => void; otherClasses?: string;
 }) => {
     return (
-        <button className="relative inline-flex h-12 width-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10" onClick={handleClick}>
+        <button className={`relative inline-flex h-12 width-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 magic-button ${otherClasses}`} onClick={handleClick}>
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className={`inline-flex h-full w-full cursor-pointer gap-2 items-center justify-center rounded-lg bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}>
-                {position === 'left' && icon}
+            <span className="inline-flex h-full w-full cursor-pointer gap-2 items-center justify-center rounded-lg bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                {position === 'left' && <span className="icon">{icon}</span>}
                 {title}
-                {position === 'right' && icon}
+                {position === 'right' && <span className="icon">{icon}</span>}
             </span>
         </button>
-    )
-}
+    );
+};
 
-export default MagicButton
+export default MagicButton;
